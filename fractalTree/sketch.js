4e401bbf51ac6ab913c6d347a3c2a3aa00e1581d
chinmay_gujar07 <chinmaygujar52@gmail.com>
2022-10-15 19:02:42 +0530

    first commit

---
+++ fractalTree/sketch.js
@@ -0,0 +1,31 @@
+var a=PI/4;
+var slider;
+function setup() {
+  createCanvas(400,400);
+  slider=createSlider(0,TWO_PI,PI/4,0.01);
+}
+ 
+function draw(){
+  background(51);
+  a=slider.value();
+  stroke(255);
+  translate(200,height);
+  branch(100);
+  
+}
+
+function branch(len)
+{
+  line(0,0,0,-len);
+  translate(0,-len);
+  if(len>4){
+    push();
+    rotate(a);
+    branch(len*0.67);
+    pop();
+    push();
+    rotate(-a);
+    branch(len*0.67);
+    pop();
+  }
+}
